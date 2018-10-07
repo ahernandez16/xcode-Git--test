50064878dd7ae48efe5f69410650a7e6c69c8972
ahernandez16 <art3her@gmail.com>
2018-10-06 18:26:09 -0700

    Initial Commit

---
+++ xcode-Git- test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xcode-Git- test
+//
+//  Created by Arturo Hernandez on 10/6/18.
+//  Copyright © 2018 Arturo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
